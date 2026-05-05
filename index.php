8b3e9890913e9642e522821eb4664c70fe8ca31d
SirioRedGiant <Oiris1992@gmail.com>
2026-05-05 15:54:08 +0200

    Add a notification for exceeding symbols and numbers only selection to prevent infinite loop and to notify user. Add a little of bootstrap.

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if (isset($_GET['length']) && !empty($_GET['length'])) {
     $generated_password = passwordGenerator($password_length, $filters, $enable_repeat);
 
     $_SESSION["generated_password"] = $generated_password;
+    $_SESSION["password_length"] = $password_length;
 
     header("Location: ./result.php");
 }
@@ -28,32 +29,40 @@ if (isset($_GET['length']) && !empty($_GET['length'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Strong Password Generator</h1>
-    <p>Qui potrai generare la tua password sicura</p>
-    <form action="index.php" method="GET">
-        <label for="length">Lunghezza password:</label>
-        <input type="number" id="length" name="length" min="4" max="70" required>
 
-        <br><br>
+<body class="bg-dark text-white p-5">
+    <div class="container text-center">
+        <h1>Strong Password Generator</h1>
+        <p>Qui potrai generare la tua password sicura</p>
+        <div>
+            <form action="index.php" method="GET">
+                <label for="length">Lunghezza password:</label>
+                <input type="number" id="length" name="length" min="4" max="70" required>
 
-        <span>Consenti ripetizioni:</span>
-        <input type="radio" name="repeat" value="1" checked> Sì
-        <input type="radio" name="repeat" value="0"> No
+                <br><br>
 
-        <br><br>
+                <span>Consenti ripetizioni:</span>
+                <input type="radio" name="repeat" value="1" checked> Sì
+                <input type="radio" name="repeat" value="0"> No
 
-        <input type="checkbox" name="filters[]" value="letters"> Lettere
-        <input type="checkbox" name="filters[]" value="numbers"> Numeri
-        <input type="checkbox" name="filters[]" value="symbols"> Simboli
+                <br><br>
 
-        <br><br>
-        <button type="submit">Genera</button>
-    </form>
+                <input type="checkbox" name="filters[]" value="letters"> Lettere
+                <input type="checkbox" name="filters[]" value="numbers"> Numeri
+                <input type="checkbox" name="filters[]" value="symbols"> Simboli
+
+                <br><br>
+                <button type="submit">Genera</button>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
--- result.php
+++ result.php
@@ -6,23 +6,51 @@ if (!isset($_SESSION["generated_password"])) {
     header("Location: ./index.php");
 }
 $password = $_SESSION["generated_password"];
+$password_length = $_SESSION["password_length"];
 
+$symbols = "!?~@#-_+<>[]{}";
+$numbers = "0123456789";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Tua Password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body>
-    <h1>La tua Password Sicura è:</h1>
-    <div>
-        <strong><?php echo $password  ?></strong>
+
+<body class="bg-dark text-white p-5">
+    <div class="container text-center">
+        <h1>La tua Password Sicura è:</h1>
+        <div>
+            <?php
+            if ((strlen($password) < $password_length) && str_contains($symbols, $password[0])) { ?>
+                <div class="alert alert-warning">
+                    <strong>Attenzione:</strong> Hai richiesto una password di <?php echo $password_length ?> caratteri senza ripetizioni.
+                    Tuttavia, il set di caratteri di soli simboli che hai scelto ne contiene solo <?php echo strlen($password) ?>.
+                    Per motivi di sicurezza, è stata generata la password più lunga possibile di soli simboli senza duplicati.
+                </div>
+            <?php
+            }
+            ?>
+            <?php
+            if ((strlen($password) < $password_length) && str_contains($numbers, $password[0])) { ?>
+                <div class="alert alert-warning">
+                    <strong>Attenzione:</strong> Hai richiesto una password di <?php echo $password_length ?> caratteri senza ripetizioni.
+                    Tuttavia, il set di caratteri di soli numeri che hai scelto ne contiene solo <?php echo strlen($password) ?>.
+                    Per motivi di sicurezza, è stata generata la password più lunga possibile di soli numeri senza duplicati.
+                </div>
+            <?php
+            }
+            ?>
+            <div class="alert alert-info">
+                <span class="fs-4"><?php echo $password ?></span>
+            </div>
+            <a href="./index.php">Torna indietro</a>
+        </div>
     </div>
-    <br>
-    <h2>Se non ti convince puoi generarne un altra</h2>
-    <p>utilizza il link per tornare al generatore</p>
-    <a href="./index.php">Torna indietro</a>
 </body>
+
 </html>
